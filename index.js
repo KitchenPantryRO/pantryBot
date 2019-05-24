b712135d186919aaa3aa68e948d515871f45cfc6
Christopher Franco Monterrosa <himahmago@gmail.com>
2019-05-24 11:12:35 -0700

    feature: PantryBot lives

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const Discord = require('discord.js');
+
+const pantryBot = new Discord.Client();
+
+pantryBot.on('ready', () => {
+  console.log('Anyone down for ET?');
+});
+
+pantryBot.on('message', message => {
+  if (message.content === 'suzz') {
+    message.reply('Did you mean Queen of the Alts?');
+  }
+});
+pantryBot.login(process.env.BOT_TOKEN);
