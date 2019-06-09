d6ea36acb2b5ccaf78e341a38114d1391aa68fba
Christopher Franco Monterrosa <himahmago@gmail.com>
2019-06-08 17:52:12 -0700

    doc: Updated help command menu

--- commands/help.js
+++ commands/help.js
@@ -1,9 +1,36 @@
+const Discord = require('discord.js');
 const help = (cmd, argTokens, message) => {
   const cmdToken = cmd.toLowerCase();
   if (cmdToken === 'help') {
-    message.reply(
-      '```👋🏼Hello! 🌿PantryBot here!\nHere are some helpful commands\nThey all start with the "$" symbol\n\n$help\n\n👾To view ET\n$et\n\n$whenis woe\n\n🐲To set your Role\nYou may choose from: Shield, Sword, Heals\n$set @username role shield\n$unset @username role shield\n\n🍳To set your class\nChose from Acolyte,Archer,Mage,Swordsman,Thief or Merchant\n$set @username class mage\n$unset @username class mage\n\n🕰To set your Timezone\nYou may choose from: PST, EST, MST or CST\n$set @username thyme pst\n$unset @username thyme pst\n\n```'
-    );
+    const embed = new Discord.RichEmbed()
+      .setTitle('🌿PantryBot Menu')
+      .setDescription(
+        "Below you'll find a list of commands, their descriptions and examples.\nWe're always open to feedback + suggestions!\nPost your questions/suggestions in #pantryBot channel"
+      )
+      .addField(
+        'COMMAND: ```$help```',
+        '👀 Description: This is the $help command\n🐱 Supported options: none\n🐚 Example:\n```$help```\n\n '
+      )
+      .addField(
+        'COMMAND: ```$et```',
+        "👀 Description: Shows MVP bosses for all ET floors.Use the arrow reacts to toggle between channels and the trashbin react to delete the message as soon as you're done\n🐱 Supported options: none\n🐚 Example:\n```$et```"
+      )
+      .addField(
+        'COMMAND: ```$whenis [EVENT]```',
+        '👀 Description: Provides time of [EVENT]\n🐱 Supported [EVENTS]: woe\n🐚 Example:\n```$whenis woe```'
+      )
+      .addField(
+        'COMMAND: ```$set @username#0123 role [ROLE]```',
+        '👀 Description: This command sets your role on discord\n🐱 Supported [ROLE]: shield,sword,heals\n🐚 Example:\n```$set @username#0123 role sword```'
+      )
+      .addField(
+        'COMMAND: ```$set @username#0123 class [CLASS]```',
+        '👀 Description:This command sets your class on discord\n🐱 Supported [CLASS]:\nAcolyte,Archer,Mage,Swordsman,Thief or Merchant\n🐚 Example:\n```$set @username#0123 class swordsman```'
+      )
+      .setColor(0x00ae86)
+      .setFooter('Powered by 🦋Technology')
+      .setTimestamp();
+    message.channel.send({ embed });
   }
 };
 
